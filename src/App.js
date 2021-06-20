7d647d7e6b0484a628d6430ab82839392ade882f
daphnemagsby <daphnemagsby@gmail.com>
2021-06-20 16:45:16 -0500

    Add Recipe Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React from 'react';
 import './App.css';
+import Recipe from './components/Recipe';
 import {useEffect, useState} from 'react';
 
 function App() {
   const APP_ID = process.env.REACT_APP_APP_ID;
   const APP_KEY = process.env.REACT_APP_APP_KEY;
   
-  const [recipes, setRecipes] =useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  
   /*useEffect runs everytime the page is rendered or something on the page is rerendered
   	add [] if only want to render at page load
   	add state variable if only want to render when state is rerendered 
   */
   useEffect(()=>{ 
-  	getRecipes();
+  	//getRecipes();
   }, []);
 
  /*make api requests with fetch when working with browser*/
@@ -30,10 +34,18 @@ function App() {
   return (
     <div className="App">
       <form className='search-form'>
-		<input type='text' className='search-bar'/>
+		<input type='text' className='search-bar' value={search} />
 		<button type='submit' className='search-button'>Search</button>
 	  </form>
-	  
+	  {recipes.map(recipe => (
+      <Recipe 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image} 
+      />
+
+    ))}
     </div>
   );
 }
