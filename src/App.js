794bddd5d4cf5ff8cd15ed3f628d79da60c3b1e2
daphnemagsby <daphnemagsby@gmail.com>
2021-06-20 17:21:51 -0500

    Add query recipes

--- src/App.js
+++ src/App.js
@@ -9,32 +9,39 @@ function App() {
   
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
   
   /*useEffect runs everytime the page is rendered or something on the page is rerendered
   	add [] if only want to render at page load
   	add state variable if only want to render when state is rerendered 
   */
   useEffect(()=>{ 
-  	//getRecipes();
-  }, []);
+  	 /*make api requests with fetch when working with browser*/
+ 	const getRecipes = async ()=>{
+	 	try{
+	 		const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+	 		const data = await res.json();
+	 		setRecipes(data.hits);
+	 	}
+	 	catch(err){
+	 		console.log("Oh No Something Went Wrong!");
+	 	}
+	}
+  	getRecipes();
+  }, [query, APP_ID, APP_KEY]);
 
- /*make api requests with fetch when working with browser*/
- const getRecipes = async ()=>{
- 	try{
- 		const res = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
- 		const data = await res.json();
- 		setRecipes(data.hits);
- 	}
- 	catch(err){
- 		console.log("Oh No Something Went Wrong!");
- 	}
-
+ const updateSearch = (e) =>{
+ 	setSearch(e.target.value);
  }
 
+ const updateQuery = (e) =>{
+ 	e.preventDefault();
+ 	setQuery(search);
+ }
   return (
     <div className="App">
-      <form className='search-form'>
-		<input type='text' className='search-bar' value={search} />
+      <form onSubmit={updateQuery} className='search-form'>
+		<input type='text' className='search-bar' value={search} onChange={updateSearch} />
 		<button type='submit' className='search-button'>Search</button>
 	  </form>
 	  {recipes.map(recipe => (
